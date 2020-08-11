20da6e16bad3976da41a122016af970a495f8527
xyzeigler <35545826+xyzeigler@users.noreply.github.com>
2020-08-11 12:46:28 -0400

    completed turtle tutorial2

--- tutorial1.py
+++ tutorial1.py
@@ -1,5 +1,51 @@
 import turtle
+import random
 
+#   tutorial 2: https://www.youtube.com/watch?v=KmziL1djFkQ
+
+tim = turtle.Turtle()
+colors = ['red','blue','green','purple','yellow','orange','black']
+
+#   set colors for turtle
+tim.color(colors[5],colors[4])  # .color(outline color, fill-in color)
+
+#   set pen width
+tim.width(5)                    # not pensize(5)?
+
+#   fill in shape with color
+tim.begin_fill()
+tim.circle(50)
+tim.end_fill()
+
+tim.penup()
+tim.forward(150)
+tim.pendown()
+
+tim.color('black','yellow')
+
+tim.begin_fill()
+for x in range(4):              # draw square
+    tim.forward(100)
+    tim.right(90)
+tim.end_fill()
+
+for x in range(5):
+    randColor=random.randrange(0,len(colors))
+    tim.color(colors[randColor],colors[random.randrange(0,len(colors))])
+    rand1=random.randrange(-300,300)
+    rand2=random.randrange(-300,300)
+    tim.penup()
+    tim.setpos(rand1,rand2)                   # cartesian plane
+    tim.pendown()
+    tim.begin_fill()
+    tim.circle(random.randrange(0,80))
+    tim.end_fill()
+
+#   we could probably define our own functions for triangles, squares, etc.
+#   so that we don't have to keep defining a new for-loop every time we want
+#   a non-circle shape to fill
+
+'''
 #   following Python Turtle Graphics Tutorial #1 - Introduction
 #   https://www.youtube.com/watch?v=p7CiFhiTdvY
 
@@ -34,3 +80,4 @@ for i in range(0,10):
     dave.backward(100)
 
 richard=turtle.Turtle()
+'''
